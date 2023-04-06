13a295fae25b5a16564af6714d9c38ef47b361b5
Anand Tharad <atharad209@gmail.com>
2023-04-06 18:33:23 +0530

    dynamic question from server

---
+++ README.md
@@ -0,0 +1,5 @@
+# QuizApp
+This is an Android Quiz App made in Java.
+<br>
+# About QuizApp 
+As of now, the Quiz App is having 10 random questions from a genre.
---
+++ app/src/main/java/com/example/quiz/QuizManager.java
@@ -0,0 +1,34 @@
+package com.example.quiz;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizManager {
+
+    private RequestQueue mRequestQueue;
+    public QuizManager(Context context ){
+
+
+    }
+
+    public void getQuizQuestions(Activity activity){
+
+
+    }
+
+}
